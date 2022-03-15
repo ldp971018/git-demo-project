98ab0584aa23068108f5513f4fc69fd5b99f8ec1
T-liudp <1846882398@qq.com>
2022-03-15 10:00:39 +0800

    提交到git远程服务器

---
+++ git-demo-project/src/com/ldp/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.ldp.demo;
+
+/**
+ * @author T-liudp
+ * @create 2022-03-15 9:48
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试项目正准备往git发布");
+        System.out.println("这是一个测试项目正准备往git发布");
+    }
+
+}
